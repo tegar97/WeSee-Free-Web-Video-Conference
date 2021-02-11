78c0c9bff285f9b536a068851a8075ee345fd9f6
Muhammad Tegar Akmal <eategar@gmail.com>
2021-02-11 16:17:45 +0700

    feature section

--- client/src/components/Section-Features/SectionFeatures.styles.ts
+++ client/src/components/Section-Features/SectionFeatures.styles.ts
@@ -5,21 +5,95 @@ export const SectionFeatureContainer = styled.section`
     
     width: 100%;
     clip-path: polygon(0 0, 100% 14%, 100% 100%, 0 82%);
-    height: 100rem;
+    height: 140rem;
     padding: 5rem 2rem;
-    margin-top: -14rem;
+    margin-top: -20rem;
     position: relative
 
 `;
 
 export const FeatureText = styled.h1`
-font-style: normal;
-font-weight: bold;
-font-size: 55px;
-line-height: 67px;
-text-align: center;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 55px;
+    line-height: 67px;
+    text-align: center;
 
-color: rgba(14, 120, 249, 0.96);
+    color: rgba(14, 120, 249, 0.96);
 
-text-shadow: 0px 10px 50px #0E78F9;
+    text-shadow: 0px 10px 50px #0E78F9;
+    transition: .5s all;
+    cursor: pointer;
+    &:hover{
+        transform: skewY(2deg) skewX(10deg) scale(1);
+        text-shadow: 0px 20px 50px #0E78F9;
+
+    }
+`
+
+export const FeatureContainer = styled.div`
+    display: grid;
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    text-align: center;
+    margin-top: 10rem/* 160px */;
+    width: '100%' !important;
+    height: ' 100%' !important ; 
+    max-height: '630px' !important ;
+    color: #fff;
+
+`
+export const FeatureScrollBar  = styled.div`
+    direction: rtl;
+    overflow:auto;
+    height: 100%;
+    width: 100%;
+    text-align: left;
+    
+`
+
+export const FeatureItemContainer = styled.div`
+display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const FeatureItemText = styled.div`
+    margin-left: 1rem;
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    
+
+`
+
+export const FeatureItemTextMain = styled.p`
+    font-size: 2rem;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 27px;
+    color: rgba(14, 120, 249, 0.96);
+    text-shadow: 0px 4px 50px #0E78F9
+`
+
+export const FeatureItemTextPrimary = styled.p`
+    font-weight: bold;
+    font-size: 13px;
+    line-height: 16px;
+
+    color: #7D7D7D;
+    margin-top: .5rem;
+`
+
+export const FeatureItem = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const FeatureIcon = styled.i`
+    color: #fff;
+    font-size: 3.5rem;
+    filter: drop-shadow(0px 10px 50px #0E78F9);
+
 `
--- client/src/components/Section-Features/SectionFeatures.tsx
+++ client/src/components/Section-Features/SectionFeatures.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { SectionFeatureContainer, FeatureText } from './SectionFeatures.styles';
+import {
+    SectionFeatureContainer,
+    FeatureText,
+    FeatureContainer,
+    FeatureItemContainer,
+    FeatureItemText,
+    FeatureItemTextMain,
+    FeatureItemTextPrimary,
+    FeatureItem,
+    FeatureIcon,
+} from './SectionFeatures.styles';
 function SectionFeatures() {
     return (
         <SectionFeatureContainer>
@@ -9,6 +19,56 @@ function SectionFeatures() {
                     And future features
                 </FeatureText>
             </div>
+            <FeatureContainer style={{ width: '100%', height: ' 100%', maxHeight: '630px' }}>
+                <FeatureItemContainer>
+                    <FeatureItem>
+                        <FeatureIcon className=" fas fa-mouse" />
+                        <FeatureItemText>
+                            <FeatureItemTextMain>One Click Go</FeatureItemTextMain>
+                            <FeatureItemTextPrimary>Create a room with just one click</FeatureItemTextPrimary>
+                        </FeatureItemText>
+                    </FeatureItem>
+                    <FeatureItem>
+                        <div>
+                            <FeatureIcon className=" fas fa-cog" />
+                        </div>
+                        <FeatureItemText>
+                            <FeatureItemTextMain> Settings Your Device</FeatureItemTextMain>
+                            <FeatureItemTextPrimary>
+                                adjust camera, audio or audio <br></br>test easily and quickly
+                            </FeatureItemTextPrimary>
+                        </FeatureItemText>
+                    </FeatureItem>
+                </FeatureItemContainer>
+                <div className="flex items-center">
+                    {' '}
+                    <img
+                        src="/ui.png"
+                        alt="ui room"
+                        style={{ filter: 'drop-shadow(0px 5px 20px #0E78F9)', justifySelf: 'center' }}
+                    />
+                </div>
+                <FeatureItemContainer>
+                    <FeatureItem>
+                        <FeatureIcon className="fas fa-comment-dots" />
+
+                        <FeatureItemText>
+                            <FeatureItemTextMain>Live Chat Message</FeatureItemTextMain>
+                            <FeatureItemTextPrimary>
+                                we ensure our clients can speak via text comfortably and quickly{' '}
+                            </FeatureItemTextPrimary>
+                        </FeatureItemText>
+                    </FeatureItem>
+                    <FeatureItem>
+                        <FeatureIcon className="fas fa-desktop" />
+
+                        <FeatureItemText>
+                            <FeatureItemTextMain>Share Your Screen</FeatureItemTextMain>
+                            <FeatureItemTextPrimary>Share your screen with one click </FeatureItemTextPrimary>
+                        </FeatureItemText>
+                    </FeatureItem>
+                </FeatureItemContainer>
+            </FeatureContainer>
         </SectionFeatureContainer>
     );
 }
